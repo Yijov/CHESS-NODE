cbf01f8599e8458b1fac98df0d9cbda469431683
Yirbett Joseph <yirbett@gmail.com>
2022-01-07 18:35:48 -0400

    solving routing errors for depployment

--- server/src/api/router/Router.ts
+++ server/src/api/router/Router.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response, NextFunction } from "express";
 const RestRouter: Router = Router();
 
 RestRouter.use("/v1", (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).json({ success: true, message: "completed", payload: {} });
+  res.status(200).json({ success: true, message: "completed", payload: "changes" });
 });
 
 export default RestRouter;
